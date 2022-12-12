03ef26dc938e43f1d8e42507ae7d5ea829c59fea
Ellina Korzhova <ellina.korzhpva@mail.ru>
2022-12-12 20:14:21 +0300

    Домашнее задание 6

---
+++ Example1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+Console.WriteLine("Введите элементы:");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+void PrintArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetPositiveNumber (int[] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++){
+
+        if (arr[i] > 0)
+        {
+        count++;
+        }
+
+    }
+    return count;
+}
+
+PrintArray(arr);
+int sumPositiveNumbers = GetPositiveNumber(arr);
+Console.WriteLine($"Количество чисел больше нуля: {sumPositiveNumbers}");
---
+++ Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+
+Console.WriteLine("Введите элементы:");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] copy = new int[arr.Length];
+for (int i = 0; i < arr.Length; i++){
+    copy[i] = arr[i];
+}
+
+ Console.WriteLine(String.Join(", ", copy));
